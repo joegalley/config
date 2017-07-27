ad27b2b6be35b7665fe037e02d16de6ef9fb8118
Joe Galley <josephgalley@gmail.com>
2017-07-26 20:27:48 -0400

    initial commit

---
+++ setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+clear
+echo -e "running setup.sh..."
+
+apt-get -qq update
+
+# git
+git --version 2>&1 >/dev/null
+GIT_INSTALLED=$?
+if [ $GIT_INSTALLED -eq 0 ];
+then
+  echo -e "\ngit is already installed [version " $(git --version)"]"
+else
+  echo -e "\ninstalling git"
+fi
+
+
+# cURL
+#sudo apt-get install curl
+
+
+# zsh (z shell)
+zsh --version 2>&1 >/dev/null
+ZSH_INSTALLED=$?
+if [ $ZSH_INSTALLED -eq 0 ];
+then
+  echo -e "\nzsh is already installed [version " $(zsh --version)"]"
+else
+  echo -e "\ninstalling zsh"
+  apt-get -y install zsh
+fi
+
+
+# docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-Linux-x86_64 > /tmp/docker-compose
+chmod +x /tmp/docker-compose
+mv /tmp/docker-compose /usr/local/bin
+echo -e "\ndocker-compose is already installed [version " $(docker-compose --version)"]"
+
+echo -e "\ndone"
+
